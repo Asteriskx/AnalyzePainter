889fe3b08743b22d26c6b2f5d5ddbc706881b259
Asteriskxx <masteriskxxx@gmail.com>
2020-02-02 21:52:14 +0900

    [add] Fix #2

---
+++ AnalyzePainter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnalyzePainter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
